7488e4848c924792d8db6445afa49561df9a41e8
catalinariquelme <catalina.riquelme.z@usach.cl>
2021-08-18 00:06:00 -0400

    Menú JFrame
    Se implementa la vista del menú principal, desde donde se podrá acceder a las diversas funcionalidades

--- src/main/java/Social/Main.java
+++ src/main/java/Social/Main.java
@@ -8,7 +8,7 @@ package Social;
 
 public class Main {
 
-    public static void main(String[] args) {        
+    public static void main(String[] args) {     
         JFrameLogin open = new JFrameLogin();
         open.setVisible(true);
         
